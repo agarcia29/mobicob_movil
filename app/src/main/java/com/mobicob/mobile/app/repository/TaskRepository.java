998777e3c540cbffb6f8666713b5a0c6fd76dbdf
90miguelangel <90miguelangel@gmail.com>
2018-08-25 16:00:14 -0500

    added observer inside taskRepository

--- app/src/main/java/com/mobicob/mobile/app/repository/TaskRepository.java
+++ app/src/main/java/com/mobicob/mobile/app/repository/TaskRepository.java
@@ -2,8 +2,10 @@ package com.mobicob.mobile.app.repository;
 
 import android.app.Application;
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
 import android.content.Context;
 import android.os.AsyncTask;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.mobicob.mobile.app.apiclient.network.RetrofitInstance;
@@ -23,15 +25,19 @@ public class TaskRepository {
 
     private TaskDao mTaskDao;
     private LiveData<List<Task>> mAllTasks;
+    private Observer<List<Task>> taskObserver;
 
     public TaskRepository(Application application) {
         MobicobDB db = MobicobDB.getDatabase(application);
         mTaskDao = db.taskDao();
+        taskObserver =  new TaskObserver(application.getApplicationContext());
+        mTaskDao.getAllTasks().observeForever(taskObserver);
+    }
 
-        if(mAllTasks==null || mAllTasks.getValue()==null || mAllTasks.getValue().isEmpty()){
-            getTaskFromWS(application.getApplicationContext());
-        }
-        mAllTasks = mTaskDao.getAllTasks();
+    @Override
+    protected void finalize() throws Throwable {
+        mTaskDao.getAllTasks().removeObserver(taskObserver);
+        super.finalize();
     }
 
     public LiveData<List<Task>> getAllTasks() {
@@ -81,6 +87,26 @@ public class TaskRepository {
         protected Void doInBackground(final Task... params) {
             mAsyncTaskDao.insert(params[0]);
             return null;
+        }
+    }
+
+    private class TaskObserver implements Observer<List<Task>>{
+
+        Context context;
+
+        public TaskObserver(Context context){
+            this.context = context;
+        }
+
+        @Override
+        public void onChanged(@Nullable final List<Task> tasks) {
+            if(tasks==null || tasks.isEmpty()) {
+                getTaskFromWS(context);
+            }
+            else{
+                mAllTasks.getValue().clear();
+                mAllTasks.getValue().addAll(tasks);
+            }
         }
     }
 }
